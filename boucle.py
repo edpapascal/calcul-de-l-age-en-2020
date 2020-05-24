6e5ba016130a5498b1728ac6f9e6d896ebef9bcc
edpapascal <65264753+edpapascal@users.noreply.github.com>
2020-05-24 08:31:12 +0100

    Create boucle.py

---
+++ boucle.py
@@ -0,0 +1,11 @@
+# une fonction pour dire a
+# l'utilisateur ou il doit s'assoir
+
+def condition(year):
+	if year<20:
+		print("espace jeune mr/me")
+	else:
+		print("espace vip mr/me")
+		
+if __name__ ==" __main__":
+	condition(30)
